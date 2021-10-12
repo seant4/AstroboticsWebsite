3d8fda7dfce5de71006525ed9932ee3a53db70a3
Alex L <38541529+alxn3@users.noreply.github.com>
2021-10-12 16:42:12 -0400

    Add subtitle

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -2,23 +2,40 @@ import React, { ReactNode } from 'react';
 
 type Props = {
   title: string;
+  subtitle?: string;
   children: ReactNode;
   containerClassName?: string;
   titleClassName?: string;
+  subtitleClassName?: string;
   contentClassName?: string;
 };
 
 const Section: React.FC<Props> = ({
   title,
+  subtitle,
   children,
   containerClassName,
   titleClassName,
+  subtitleClassName,
   contentClassName,
 }) => {
   return (
     <div className={`p-4 ${containerClassName || ''}`}>
-      <h1 className={`text-3xl font-bold ${titleClassName || ''}`}>{title}</h1>
-      <div className={`text-lg font-semibold ${contentClassName || ''}`}>
+      <h1
+        className={`text-4xl font-semibold text-center ${titleClassName || ''}`}
+      >
+        {title}
+      </h1>
+      {subtitle && (
+        <h2
+          className={`text-2xl font-light text-center ${
+            subtitleClassName || ''
+          }`}
+        >
+          {subtitle}
+        </h2>
+      )}
+      <div className={`text-xl font-regular text-gray-800 mt-2 ${contentClassName || ''}`}>
         {children}
       </div>
     </div>
